76c8d9a4fe379928ea53ccb8a90b5e358d6af669
nadiya123123 <nadiya.ns.99@gmail.com>
2023-06-05 15:01:48 +0530

    final

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    dob = models.DateField()
+    phone_number = models.CharField(max_length=20)
--- user_app/views.py
+++ user_app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
 from django.contrib import messages, auth
+from .models import UserProfile
 
 # Create your views here.
 
@@ -17,7 +18,6 @@ def login(request):
             messages.info(request, "Invalid username or password")
             return redirect('login')
     return render(request, "login.html")
-
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -36,10 +36,20 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, password=password)
                 user.save()
+
+                UserProfile.objects.create(
+                    user=user,
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    address=request.POST['address'],
+                    dob=request.POST['dob'],
+                    phone_number=request.POST['phone_number']
+                )
                 return redirect('login')
         else:
             messages.info(request, "Passwords do not match")
             return redirect('register')
+
     return render(request, "register.html")
 
 
